88a8335c54b224b3616fe751cd2669560504f260
Sienna Bates <the.strawberry.almond.milk@gmail.com>
2024-01-07 13:52:48 -0500

    adding maps and sets exercise

---
+++ Maps and Sets Exercise.js
@@ -0,0 +1,39 @@
+new Set([1, 1, 2, 2, 3, 4]);
+// [1,2,3,4]
+
+[...new Set("referee")].join("");
+// "ref"
+
+let m = new Map();
+m.set([1, 2, 3], true);
+m.set([1, 2, 3], false);
+
+/*
+  0: {Array(3) => true}
+  1: {Array(3) => false}
+*/
+
+function hasDuplicate(arr) {
+  if (new Set(arr).size !== arr.length) {
+    return true;
+  } else return false;
+}
+
+function isVowel(char) {
+  return "aeiou".includes(char);
+}
+
+function vowelCount(str) {
+  const vowelMap = new Map();
+  for (let char of str) {
+    let lowerCaseChar = char.toLowerCase();
+    if (isVowel(lowerCaseChar)) {
+      if (vowelMap.has(lowerCaseChar)) {
+        vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+      } else {
+        vowelMap.set(lowerCaseChar, 1);
+      }
+    }
+  }
+  return vowelMap;
+}
